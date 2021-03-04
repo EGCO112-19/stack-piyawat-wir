ab5f99f864d73a90e0b54deb3fb4eef8c074f3fd
piyawatwir <piyawat.wir@student.mahidol.edu>
2021-03-04 19:09:19 +0000

    added parentheses check

--- main.c
+++ main.c
@@ -4,26 +4,34 @@
 #include "stack.h"
 
 int main(int argc, char **argv){
-  printf("Checking the parentheses in argv arguments\n");
-  int i,N,j;
-  
-  Stack s;
+    printf("Checking the parentheses in argv arguments\n");
 
+    int i, j;
+    char c;
+    Stack s;
+    s.size = 0;
+    s.top = NULL;
 
- 
-  for(i=1;i<argc;i++){
-   
-     for(j=0;j<strlen(argv[i]);j++){
-       /* Use stack to help with the parentheses*/
+    for (i=1; i < argc; i++) {
+        for (j=0; j < strlen(argv[i]) && j != -1; j++) {
+            c = argv[i][j];
+            switch(c) {
+                case '{':
+                case '[': push(&s, c); break;
 
-
-     }
-
-
-  }
-
-
-
-
-   return 0;
+                case ']':
+                    if (pop(&s) != '[') j = -2;
+                    break;
+                case '}':
+                    if (pop(&s) != '{') j = -2;
+                    break;
+            }
+        }
+        if (s.size != 0 || j < 0)
+            printf("The parentheses do not match for %s\n", argv[i]);
+        else
+            printf("The parentheses match successfully for %s\n", argv[i]);
+        pop_all(&s);
+    }
+    return 0;
 }
--- stack.h
+++ stack.h
@@ -11,4 +11,31 @@ typedef Stack * StackPtr;
 void push(StackPtr s, char value);
 char pop(StackPtr s);
 void pop_all(StackPtr s);
+
+
+void push(StackPtr s, char value) {
+    Node *new_node = (Node*) malloc(sizeof(Node));
+    if (new_node) { //memory available
+        new_node->data = value;
+        new_node->nextPtr = s->top;
+        s->top = new_node;
+        s->size++;
+    }
+}
+char pop(StackPtr s) {
+    Node *top = s->top;
+    char value = 0;
+    if (s->top != NULL) { //Stack not empty
+        value = top->data;
+        s->top = top->nextPtr;
+        free(top);
+        s->size--;
+    }
+    return value;
+}
+void pop_all(StackPtr s) {
+    while (s->size > 0)
+        printf("popping %c\n", pop(s));
+}
+
 #endif
